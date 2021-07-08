e5b057fbb061b70fb9566dc0970628afa1905b1c
Cezar Gonzalez Filho <cezar.gfilho@gmail.com>
2021-07-08 13:33:25 -0300

    implementacao inicial de seguranca

---
+++ alura-forum-inicial/src/main/java/br/com/alura/forum/config/seguranca/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.config.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	// Configuracoes de autenticacao (acesso/login)
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+	}
+
+	// Configuracoes de autorizacao (url, perfil de acesso)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated();
+	}
+
+	// Configuracoes de recursos estaticos (css, js, imgs, ect...)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+
+}
